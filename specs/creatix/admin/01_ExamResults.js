dc310d2bc570252d8ba01343bf42abb5b9880603
Deniz Skantz <denizskantz@hotmail.com>
2019-01-01 17:42:02 -0500

    adding a new exam test, get added to the actual database

--- specs/creatix/admin/01_ExamResults.js
+++ specs/creatix/admin/01_ExamResults.js
@@ -3,13 +3,17 @@ const logout = $('.btn.btn-danger');
 const addExam = $$('.btn.btn-light').first();
 const examPage = $('.publiname');
 const inputs = $$('.form-control');
+const submit = $('[value=\'Submit!\']');
+const results = $('.headline');
+const tablerows = element.all(by.css('table.table-hover tbody tr'));
 const EC = protractor.ExpectedConditions;
+
 // const admin = require('../Admin');
 // import admin from '../Admin';
 
 
 describe('00_Admin Adding Exam Results:', () => {
-  it('01_Add new exam page showing', async () => {
+  it('01_Add a new exam', async () => {
     expect (await collapsed.isPresent()).toBe(true, 'Dashboard is not present');
     await addExam.click();
     await browser.get('http://www.ilkeciritci.com/exam-results/new');
@@ -20,8 +24,12 @@ describe('00_Admin Adding Exam Results:', () => {
     await inputs.get(2).sendKeys('Deniz');
     await inputs.get(3).sendKeys('Testing');
     await inputs.get(4).sendKeys('99');
+    await submit.click();
   });
-  xit('04_Select exam results and check if the admin view is present', async () => {
+  it('03_Check if the new exam result is showing', async () => {
+    await browser.get('http://www.ilkeciritci.com/exam-results/admin');
+    browser.wait(EC.visibilityOf(results), 5000);
+    expect (await tablerows.count()).toEqual(5, 'New exam is not added');
     // await addExam.click()
     // await dashboard.click();
     // await login.click();
